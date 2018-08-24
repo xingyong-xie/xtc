2230a54e872b46fa55176f47b14c7dfc911c463d
xingyong xie <xingyong_xie@sphinx.work>
2018-08-24 13:27:40 +0800

    update home page

--- xtc/settings.py
+++ xtc/settings.py
@@ -100,5 +100,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+
+MEDIA_URL =  '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "images")
+
+
+print STATIC_ROOT
+
--- xtc/urls.py
+++ xtc/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import template_as_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', template_as_view("index.html")),
+    url(r'^index_demo.html', template_as_view("index-demo.html")),
     url(r'^admin/', include(admin.site.urls)),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
